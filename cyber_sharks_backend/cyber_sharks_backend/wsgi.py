f3c6f89339add043f2f808630fd80dc2efade383
duong-28 <thuyduong.kt.ftu@gmail.com>
2025-01-18 11:14:20 +1000

    replaced with cyber_sharks_backend

--- cyber_sharks_backend/cyber_sharks_backend/wsgi.py
+++ cyber_sharks_backend/cyber_sharks_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for crowdfunding project.
+WSGI config for cyber_sharks_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crowdfunding.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cyber_sharks_backend.settings')
 
 application = get_wsgi_application()
