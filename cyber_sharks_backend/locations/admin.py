82ab1a1b790d7353ca983bcc0a04b5a804bc7239
MACKENNADRIES <138457102+MACKENNADRIES@users.noreply.github.com>
2025-01-18 18:30:51 +1000

    made admin more readable

--- cyber_sharks_backend/locations/admin.py
+++ cyber_sharks_backend/locations/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Location
 
 
-@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
+    model = Location
     list_display = ("city_name",)
     search_fields = ("city_name",)
+
+
+admin.site.register(Location, LocationAdmin)
